5bdfcced50cbac9d80aaefa87c427385064435a3
codebendr-product-designer <codebendr@gmail.com>
2021-08-05 10:11:45 +0000

    Initial Commit

---
+++ Petition/PetitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetitionApp.swift
+//  Petition
+//
+//  Created by Evans Domina Attafuah on 05/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PetitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
