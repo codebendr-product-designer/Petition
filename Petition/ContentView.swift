1f39c449629a6c40a6b5186ada31bbe2df6c61ec
codebendr-product-designer <codebendr@gmail.com>
2021-08-07 14:22:42 +0000

    integrated API and added the most basic UI

--- Petition/ContentView.swift
+++ Petition/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var petitons = [Petition]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(petitons) {
+                Text("\($0.title): ").bold() +
+                Text($0.body)
+                
+            }
+            .navigationTitle("Pettiton")
+            .task {
+                do {
+                    inbox = try await inbox()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func inbox() async throws -> [Petition] {
+        try await URLSession.shared.decode(from: URL(string: "https://hws.dev/petitions.json")!)
     }
 }
 
