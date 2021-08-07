04c3d5124dc1e05527228562cf5d487bfbd26c0e
codebendr-product-designer <codebendr@gmail.com>
2021-08-07 14:12:21 +0000

    included url session extension

---
+++ Petition/URLSession+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  URLSession+Extension.swift
+//  URLSession+Extension
+//
+//  Created by Evans Domina Attafuah on 04/08/2021.
+//
+
+import SwiftUI
+
+extension URLSession {
+    func decode<T: Decodable> (
+        _ type: T.Type = T.self,
+        from url: URL,
+        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
+        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
+    ) async throws -> T {
+        //try and wait for data
+        let (data, _) = try await data(from: url)
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+        decoder.dataDecodingStrategy = dataDecodingStrategy
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        
+        let decoded = try decoder.decode(T.self, from: data)
+        return decoded
+    
+    }
+}
+
+struct Message: Codable, Identifiable {
+    let id: Int
+    let user: String
+    let text: String 
+}
+
+
