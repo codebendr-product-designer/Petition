3ffe9f2ccb18d8bf3a2b2a168c25dc5c547eb513
codebendr-product-designer <codebendr@gmail.com>
2021-08-08 00:20:54 +0000

    added color for progress

--- Petition/ContentView.swift
+++ Petition/ContentView.swift
@@ -83,7 +83,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().ignoresSafeArea(.all)
+        ContentView()
+            .environment(\.colorScheme, .dark)
+            .ignoresSafeArea(.all)
     }
 }
 
