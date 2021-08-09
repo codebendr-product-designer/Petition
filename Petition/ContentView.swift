e955302688ace063612ab232c7f6a285beb65e1f
codebendr-product-designer <codebendr@gmail.com>
2021-08-09 15:28:08 +0000

    added petition detail view preview

--- Petition/ContentView.swift
+++ Petition/ContentView.swift
@@ -20,11 +20,11 @@ struct ContentView: View {
                     NavigationLink(destination: PetitionDetailView(petition: petition)) {
                         PetitionView(petition: petition)
                     }
+                    .listStyle(.insetGrouped)
                 }
                 .task {
                     do {
                         petitions = try await petitions().shuffled()
-                        
                     } catch {
                        print(error.localizedDescription)
                     }
@@ -40,16 +40,25 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    static let petition = Petition(id: "2715183", title: "Stop the witch hunt of the duly elected president of the U.S.A  and obstructing the president's work.", body: "What they are doing to Donald Trump is 'really a witch hunt'.  After two years, and no findings of collusion or obstruction, the investigation has been derailed into an ongoing, non-ending witch hunt, obliterating,  intimidating, and bankrupting staffers and family, setting traps in the pursuit of revenge for having had won an election fairly and against all odds. A blind eye has been applied to the real corrupt agents of the federal government allowing them to retire with pensions and untouched by justice. We demand this persecution be stopped and he is allowed to govern for the benefit of our national security without further distractions. We demand that the real corrupt colluders and obstructers are brought to justice and that their benefits be removed. We support our president.", signatureCount: Int.random(in: 149..<40000), signatureThreshold: 100000)
+    
     static var previews: some View {
         Group {
-//            ContentView()
-//                .environment(\.colorScheme, .dark)
-//            .ignoresSafeArea(.all)
             
             ContentView()
                 .environment(\.colorScheme, .light)
                 .ignoresSafeArea(.all)
+            
             ContentView()
+                .environment(\.colorScheme, .dark)
+                .ignoresSafeArea(.all)
+            
+            PetitionDetailView(petition: petition)
+                .environment(\.colorScheme, .light)
+                .ignoresSafeArea(.all)
+            
+            PetitionDetailView(petition: petition)
                 .environment(\.colorScheme, .dark)
                 .ignoresSafeArea(.all)
         }
@@ -75,9 +84,6 @@ struct TitleView: View {
 struct PetitionView: View {
     let petition: Petition
     let emoji = ["🔐","🧘🏿‍♂️","🧨","💡","🚁","🎯","🎨", "🛺", "🚦"].randomElement()!
-    var isBelowThreshold: Bool {
-        petition.signatureCount < 40000
-    }
     
     var body: some View {
         HStack {
@@ -90,75 +96,64 @@ struct PetitionView: View {
                     .foregroundColor(.title)
                     .bold()
                 
-                HStack {
-                    
-                    Image(systemName: "person.3.sequence.fill")
-                        .foregroundColor(.accent)
-                        .padding(.trailing, 5)
-                    
-                    Text("\(petition.signatureCount)")
-                        .foregroundColor(isBelowThreshold ? .pink : .progess).font(.body).bold()
-                        .padding(.all, -6)
-                    
-                    Text("/ \(petition.signatureThreshold)")
-                        .foregroundColor(.accent)
-                        .bold()
-                    
-                    Spacer()
-                    
-                }
-                .padding(.top, 1)
-                
-                ProgressView(value: Double(petition.signatureCount), total: Double(petition.signatureThreshold))
-                    .tint(isBelowThreshold ? .pink : .progess)
-                    .progressViewStyle(.linear)
+                PetitionProgressView(petition: petition)
+          
                 
             }
         }
         .listRowBackground(Color.background)
     }
+}
+
+struct PetitionProgressView: View {
+    let petition: Petition
+    var isBelowThreshold: Bool {
+        petition.signatureCount < 40000
+    }
     
- 
-    
-    
+    var body: some View {
+        HStack {
+            
+            Image(systemName: "person.3.sequence.fill")
+                .foregroundColor(.accent)
+                .padding(.trailing, 5)
+            
+            Text("\(petition.signatureCount)")
+                .foregroundColor(isBelowThreshold ? .pink : .progess).font(.body).bold()
+                .padding(.all, -6)
+            
+            Text("/ \(petition.signatureThreshold)")
+                .foregroundColor(.accent)
+                .bold()
+            
+            Spacer()
+            
+        }
+        .padding(.top, 1)
+        
+        ProgressView(value: Double(petition.signatureCount), total: Double(petition.signatureThreshold))
+            .tint(isBelowThreshold ? .pink : .progess)
+            .progressViewStyle(.linear)
+    }
 }
 
 struct PetitionDetailView: View {
     let petition: Petition
     
     var body: some View {
-        HStack {
+        VStack {
             
-            Text("🔐").font(.largeTitle)
+            Text("🔐").font(.system(size: 48))
             
             VStack(alignment: .leading) {
                 
                 Text(petition.title)
-                    .foregroundColor(.title)
                     .bold()
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.title)
+                    
                 
-                HStack {
-                    
-                    Image(systemName: "person.3.sequence.fill")
-                        .foregroundColor(.accent)
-                        .padding(.trailing, 5)
-                    
-                    Text("540")
-                        .foregroundColor(.green).font(.body).bold()
-                        .padding(.all, -6)
-                    
-                    Text("/ 340,987")
-                        .foregroundColor(.accent)
-                        .bold()
-                    
-                    Spacer()
-                    
-                }
-                .padding(.top, 1)
-                
-                ProgressView(value: 0.2)
-                    .tint(.progess)
-                    .progressViewStyle(.linear)
+                PetitionProgressView(petition: petition)
                 
             }
         }
